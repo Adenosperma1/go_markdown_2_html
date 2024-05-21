13f90f7f4b7e5a7f0e22d172a6ee350636552643
Brendan Wilde <44901073+Adenosperma1@users.noreply.github.com>
2024-05-21 23:12:27 +1000

    ...

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_markdown_2_html.go
 
 go 1.21.6
+
+require github.com/gomarkdown/markdown v0.0.0-20240419095408-642f0ee99ae2
--- go_markdown_2_html.go
+++ go_markdown_2_html.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/gomarkdown/markdown"
 )
 
 func main(){
 	fmt.Println("Hi")
+	file := "test.md"
+	content, err := os.ReadFile(file)
+
+	if err != nil {
+		log.Fatalf("%s file not found", file)
+	}
+
+	html := markdown.ToHTML(content, nil, nil)
+
+	fmt.Println(string(html))
+
+	fileOut := "test.html"
+	errOut := os.WriteFile(fileOut, html, 0644)
+	if errOut != nil {
+		log.Fatalf("Couldn't create the file %s ", fileOut)
+	}
+
+	fmt.Printf("HTML file called %s created", fileOut)
 }
